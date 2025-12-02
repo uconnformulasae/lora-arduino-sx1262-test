9aa2d0f9b5bce9c63801a7b19f34d7ad413efccb
ckury <155270699+ckury@users.noreply.github.com>
2025-12-02 17:25:07 -0500

    Changed defaults a bit

--- src/config.h
+++ src/config.h
@@ -8,8 +8,8 @@
 
 #define d_frequency 915.0
 #define d_bandwidth 500.0
-#define d_spreading_factor 6
-#define d_coding_rate 5
+#define d_spreading_factor 7
+#define d_coding_rate 1
 #define d_sync_word 0x34
 #define d_output_power 2
 #define d_preamble_length 20
