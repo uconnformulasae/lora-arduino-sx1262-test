20852ccee30db4b5d1136b1a1e98777ee2b7777d
ckury <155270699+ckury@users.noreply.github.com>
2026-02-04 13:27:38 -0500

    Added settings struct

--- src/utils.h
+++ src/utils.h
@@ -3,16 +3,38 @@
  *  Description: Holds basic utility functions
  * 
  *  Author: Charlie K.
- *  Date: 11/29/25
+ *  Date: 2/4/2026
  */
 
 #ifndef UTILS_H
 #define UTILS_H
 
 #include <Arduino.h>
+#include <RadioLib.h>
 
 #define EPSILON 0.001f
 
+typedef struct settings {
+    uint32_t signature;
+    float bw;
+    uint8_t sf;
+    uint8_t cr;
+    uint8_t pwr;
+    uint16_t sw;
+    uint16_t pre;
+    uint32_t seed; 
+} settings;
+
+void serialCommandParser(String input, settings radset);
+
+void eepromSaveSettings(settings radset);
+
+void serialError(const char* msg, char* value);
+
+void serialError(const char* msg, char* value);
+
 uint8_t isClose(float a, float b);
+
+void userInitializeSX1262(SX1262 radio, settings radset);
 
 #endif
