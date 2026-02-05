11442755c458f2aa4796d97038ec009ad14b9218
ckury <155270699+ckury@users.noreply.github.com>
2026-02-04 23:22:12 -0500

    Moved initialization into utils and added some error helper functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,8 +8,35 @@
 
 #include <Arduino.h>
 #include <utils.h>
+#include <EEPROM.h>
+
+
+void serialError(const char* msg, char* value) {
+    Serial.print(msg);
+    Serial.println(value);
+}
+
+void serialError(const char* msg, int value) {
+    Serial.print(msg);
+    Serial.println(value);
+}
+
+void eepromSaveSettings(settings radset) {
+    EEPROM.put(0, radset);
+    return;
+}
 
 uint8_t isClose(float a, float b) { // Float comparison cuz its difficult with approximations and whatnot. Honestly, AI helped me a decent bit with this.
     if (fabs(a - b) < EPSILON) {return 1;}
     else {return 0;}
+}
+
+void userInitializeSX1262(SX1262 radio, settings radset, int rf_sw_pin) {
+    int radio_state = radio.begin(radset.base_freq, radset.bw, radset.sf, radset.cr, radset.sw, radset.pwr, radset.pre, 1.7F /* Added TCXO voltage for troubleshooting, may not be nessesary*/);
+
+    if (radio_state != RADIOLIB_ERR_NONE) {
+        serialError("SX1262 Initialization Error:", radio_state);
+    }
+
+    radio.setRfSwitchPins(rf_sw_pin, -1);
 }
--- src/utils.h
+++ src/utils.h
@@ -15,26 +15,27 @@
 #define EPSILON 0.001f
 
 typedef struct settings {
-    uint32_t signature;
+    //uint32_t signature; For future use
+    float base_freq;
     float bw;
     uint8_t sf;
     uint8_t cr;
-    uint8_t pwr;
     uint16_t sw;
+    uint8_t pwr;
     uint16_t pre;
     uint32_t seed; 
 } settings;
 
-void serialCommandParser(String input, settings radset);
+// void serialCommandParser(String input, settings radset); For future use
 
 void eepromSaveSettings(settings radset);
 
 void serialError(const char* msg, char* value);
 
-void serialError(const char* msg, char* value);
+void serialError(const char* msg, int value);
 
 uint8_t isClose(float a, float b);
 
-void userInitializeSX1262(SX1262 radio, settings radset);
+void userInitializeSX1262(SX1262 radio, settings radset, int rf_sw_pin);
 
 #endif
